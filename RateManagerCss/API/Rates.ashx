6d8c06535040a078bf18b85b92ddff8941d659ee
Chepe <josealberto@univisit.com>
2019-05-03 14:02:29 -0600

    XX

---
+++ RateManagerCss/API/Rates.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="VB" CodeBehind="Rates.ashx.vb" Class="RateManager.HdrFaresCatalogue" %>
---
+++ RateManagerCss/API/Rates.ashx.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Web
+Imports System.Web.Services
+
+Public Class Rates
+    Implements System.Web.IHttpHandler
+
+    Sub ProcessRequest(ByVal context As HttpContext) Implements IHttpHandler.ProcessRequest
+
+        context.Response.ContentType = "text/plain"
+        context.Response.Write("¡!")
+
+
+
+    End Sub
+
+    ReadOnly Property IsReusable() As Boolean Implements IHttpHandler.IsReusable
+        Get
+            Return False
+        End Get
+    End Property
+
+End Class
