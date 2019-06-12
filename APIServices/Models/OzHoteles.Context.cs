466b2412c61cf070316cc51dc9f433ac8fff1b2e
José Hernández Morales <pepe@internetpowerhotel.com>
2019-06-11 22:11:27 -0600

    Impuesto en info basica de hotel

--- APIServices/Models/OzHoteles.Context.cs
+++ APIServices/Models/OzHoteles.Context.cs
@@ -32,12 +32,12 @@ namespace APIServices.Models
         public virtual DbSet<Indice> Indice { get; set; }
         public virtual DbSet<vDayRateDetail> vDayRateDetail { get; set; }
         public virtual DbSet<vDayRates> vDayRates { get; set; }
-        public virtual DbSet<vHotelBasicInfo> vHotelBasicInfo { get; set; }
         public virtual DbSet<vHotelRoom> vHotelRoom { get; set; }
         public virtual DbSet<vReservation> vReservation { get; set; }
         public virtual DbSet<vRoomInventory> vRoomInventory { get; set; }
         public virtual DbSet<ContratosNR> ContratosNR { get; set; }
         public virtual DbSet<vHotelByUser> vHotelByUser { get; set; }
         public virtual DbSet<vHotelPlan> vHotelPlan { get; set; }
+        public virtual DbSet<vHotelBasicInfo> vHotelBasicInfo { get; set; }
     }
 }
