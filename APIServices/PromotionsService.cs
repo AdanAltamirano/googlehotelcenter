f1099382cedc9aa0fbcf30e96d176a1d651cf374
Chepe <josealberto@univisit.com>
2020-04-13 16:48:56 -0600

    Avance

---
+++ APIServices/PromotionsService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using APIServices.Models;
+using APIServices.Models.DTO;
+
+namespace APIServices
+{
+    class PromotionsService
+    {
+        //public IEnumerable<Offer> FindOffers(int hotelId)
+        //{
+        //    using (OzHotelesEntities db = new OzHotelesEntities())
+        //    {
+        //        var result = db.
+        //    }
+                
+
+        //    return Offers;
+        //}
+    }
+}
