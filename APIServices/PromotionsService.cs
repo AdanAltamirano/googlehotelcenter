e9b79ab5f313e003037b474e3f335f241646877e
Chepe <josealberto@univisit.com>
2020-04-27 21:05:34 -0600

    OfferAdd para listo para pruebas

--- APIServices/PromotionsService.cs
+++ APIServices/PromotionsService.cs
@@ -33,7 +33,7 @@ namespace APIServices
                                Amount = o.DiscountApplicationType == 2 ? o.Discount : 0,
                                Percent = o.Discount == 1 ? o.Discount : 0,
                                ApplicationMode = OfferDiscount.GetApplicationMode(o.DiscountApplicationMode)
-                           },                           
+                           },
                            ApplicableFor = Offer.GetApplicableFor(o.HotelId, o.PromotionCode),
                            Rule = new OfferRule()
                            {
@@ -68,89 +68,119 @@ namespace APIServices
 
         public KeyValuePair<string, string> Add(Offer offer)
         {
+
             OzHotelesEntities db = new OzHotelesEntities();
             using (System.Data.Entity.DbContextTransaction transaction = db.Database.BeginTransaction())
             {
-
-                //Reglas
-                var rules = new RatesPlanRules() {
-                    idRule = offer.Rule.Id,
-                    IdHotel = offer.HotelId,
-                    ReqVerification = true,
-                    GuarDep = "N",
-                    RateRulesDefault = true,
-                    CancelPriorHours = offer.Rule.CancelPenalty.OffsetTimeUnit == OfferCancelPenaltyOffsetTimeUnit.Hours ? offer.Rule.CancelPenalty.OffsetTimeUnitMiltiplier : null,
-                    CancelPriorDays = offer.Rule.CancelPenalty.OffsetTimeUnit == OfferCancelPenaltyOffsetTimeUnit.Days ? offer.Rule.CancelPenalty.OffsetTimeUnitMiltiplier : null,
-                    CancelPriorSpecificT = offer.Rule.CancelPenalty.OffsetTimeUnit == OfferCancelPenaltyOffsetTimeUnit.SpecificTimeOfDay ? offer.Rule.CancelPenalty.SpecificOffsetTime : null,
-                    AdvBooking = offer.Rule.MinAdvanceBookingOffset != 0 ? offer.Rule.MinAdvanceBookingOffset : null,
-                    MaxAdvBooking = offer.Rule.MaxAdvanceBookingOffset != 0 ? offer.Rule.MaxAdvanceBookingOffset : null,
-                    MaxDias = offer.Rule.MaxLOS != 0 ? offer.Rule.MaxLOS : null,
-                    MinDias = offer.Rule.MinLOS != 0 ? offer.Rule.MinLOS : null,
-                    NoArrivos = Utilities.GetDaysOfWeekString(offer.Rule.NoArrivals),
-                    PromoSpecificDays = Utilities.GetDaysOfWeekString(offer.Rule.ApplyDays),
-                    PromoStartDate = offer.StartDate,
-                    PromoEndDate = offer.EndDate
-                };
-
-                var deal = new ratesPlanDeal()
+                try
                 {
-                    idRatePlan = offer.Id,
-                    idHotel = offer.HotelId,
-                    FechaInicio = offer.Rule.BookingWindow.StartDate,
-                    FechaFin = offer.Rule.BookingWindow.EndDate,
-                    HoraInicio = offer.Rule.BookingWindow.StartHour,
-                    HoraFin = offer.Rule.BookingWindow.EndHour,
-                };
-
-                //Aplicable para
-                var rooms = offer.ApplicableFor.Rooms.Select(r =>
-                    new Promociones_TipoHabitacionHotel() {
-                        IdHotel = offer.HotelId,
-                        IdPromocion = offer.Id,
-                        IdTipoHabitacionHotel = r
-                    }
-                ).ToList();
-
-                var ratesplan = offer.ApplicableFor.RatesPlan.Select(r =>
-                    new Promociones_RatePlan()
+                    var exist = db.RatesPlan.FirstOrDefault(x => x.idRatePlan == offer.Id && x.IdHotel == offer.HotelId);
+                    if (exist == null)
                     {
-                        IdHotel = offer.HotelId,
-                        IdPromocion = offer.Id,
-                        IdRatePlan = r
-                    }
-                ).ToList();
 
-                //RatePlan
-                var rp = new RatesPlan()
+                        //Reglas
+                        var rule = new RatesPlanRules()
+                        {
+                            //idRule = offer.Rule.Id,
+                            IdHotel = offer.HotelId,
+                            ReqVerification = true,
+                            GuarDep = "N",
+                            RateRulesDefault = true,
+                            CancelPriorHours = offer.Rule.CancelPenalty.OffsetTimeUnit == OfferCancelPenaltyOffsetTimeUnit.Hours ? offer.Rule.CancelPenalty.OffsetTimeUnitMiltiplier : null,
+                            CancelPriorDays = offer.Rule.CancelPenalty.OffsetTimeUnit == OfferCancelPenaltyOffsetTimeUnit.Days ? offer.Rule.CancelPenalty.OffsetTimeUnitMiltiplier : null,
+                            CancelPriorSpecificT = offer.Rule.CancelPenalty.OffsetTimeUnit == OfferCancelPenaltyOffsetTimeUnit.SpecificTimeOfDay ? offer.Rule.CancelPenalty.SpecificOffsetTime : null,
+                            AdvBooking = offer.Rule.MinAdvanceBookingOffset != 0 ? offer.Rule.MinAdvanceBookingOffset : null,
+                            MaxAdvBooking = offer.Rule.MaxAdvanceBookingOffset != 0 ? offer.Rule.MaxAdvanceBookingOffset : null,
+                            MaxDias = offer.Rule.MaxLOS != 0 ? offer.Rule.MaxLOS : null,
+                            MinDias = offer.Rule.MinLOS != 0 ? offer.Rule.MinLOS : null,
+                            NoArrivos = Utilities.GetDaysOfWeekString(offer.Rule.NoArrivals),
+                            PromoSpecificDays = Utilities.GetDaysOfWeekString(offer.Rule.ApplyDays),
+                            PromoStartDate = offer.StartDate,
+                            PromoEndDate = offer.EndDate,
+                            idDiccionarioPoliticaCancelacionReview = Dictionary.Insert(offer.Rule.CancelPenalty.ShortDescription.Esp, offer.Rule.CancelPenalty.ShortDescription.Eng),
+                            idDiccionarioPoliticaCancelacionFull = Dictionary.Insert(offer.Rule.CancelPenalty.DetailedDescription.Esp, offer.Rule.CancelPenalty.DetailedDescription.Eng)
+                        };
+                        db.RatesPlanRules.Add(rule);
+
+                        //RatePlan
+                        var rp = new RatesPlan()
+                        {
+                            idRatePlan = offer.Id,
+                            Description = offer.Name.Esp,
+                            IdHotel = offer.HotelId,
+                            Segment = "R",
+                            Name = offer.Name.Esp,
+                            RateGDS = false,
+                            RatePortal = true,
+                            RateUnip = false,
+                            codigotarifa = offer.Id,
+                            DescPromotion = offer.Discount.Percent != 0 ? offer.Discount.Percent : offer.Discount.Amount != 0 ? offer.Discount.Amount : null,
+                            DaysFree = (short)offer.Discount.NightsDiscounted,
+                            DaysFreeType = false,
+                            RateADS = false,
+                            gdsApply = "NNNN",
+                            ComADS = 0,
+                            ComGDS = 0,
+                            ComOnePage = 0,
+                            ComPortal = 0,
+                            WaitListAvailable = false,
+                            hotelPayment = false,
+                            IsPromo = true,
+                            DiscountLevel = GetDiscountApplicationMode(offer.Discount.ApplicationMode),
+                            TipoDescuento = 1, //Porcentaje. TODO: Monto
+                            IdDictionaryDescription = Dictionary.Insert(offer.Name.Esp, offer.Name.Eng),
+                            IdDiccShortDesc = Dictionary.Insert(offer.Description.Esp, offer.Description.Eng),
+                            idDiccPromoDesc = Dictionary.Insert("", ""), //Valor Agregado
+                            Deleted = false,
+                            idRule = rule.idRule
+                        };
+                        db.RatesPlan.Add(rp);
+
+                        var deal = new ratesPlanDeal()
+                        {
+                            idRatePlan = offer.Id,
+                            idHotel = offer.HotelId,
+                            FechaInicio = offer.Rule.BookingWindow.StartDate,
+                            FechaFin = offer.Rule.BookingWindow.EndDate,
+                            HoraInicio = offer.Rule.BookingWindow.StartHour,
+                            HoraFin = offer.Rule.BookingWindow.EndHour,
+                        };
+                        db.ratesPlanDeal.Add(deal);
+
+                        //Aplicable para
+                        var rooms = offer.ApplicableFor.Rooms.Select(r =>
+                            new Promociones_TipoHabitacionHotel()
+                            {
+                                IdHotel = offer.HotelId,
+                                IdPromocion = offer.Id,
+                                IdTipoHabitacionHotel = r
+                            }
+                        ).ToList();
+                        db.Promociones_TipoHabitacionHotel.AddRange(rooms);
+
+                        var ratesplan = offer.ApplicableFor.RatesPlan.Select(r =>
+                            new Promociones_RatePlan()
+                            {
+                                IdHotel = offer.HotelId,
+                                IdPromocion = offer.Id,
+                                IdRatePlan = r
+                            }
+                        ).ToList();
+                        db.Promociones_RatePlan.AddRange(ratesplan);
+
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    else
+                    {
+                        return new KeyValuePair<string, string>("0", "Add: " + "El ID de promoción " + offer.Id + " ya existe.");
+                    }
+                }
+                catch (Exception e)
                 {
-                    idRatePlan = offer.Id,
-                    Description = offer.Name.Esp,
-                    IdHotel = offer.HotelId,
-                    Segment = "R",
-                    Name = offer.Name.Esp,
-                    RateGDS = false,
-                    RatePortal = true,
-                    RateUnip = false,
-                    codigotarifa = offer.Id,
-                    DescPromotion = offer.Discount.Percent != 0 ? offer.Discount.Percent : offer.Discount.Amount != 0 ? offer.Discount.Amount : null,
-                    DaysFree = (short)offer.Discount.NightsDiscounted,
-                    DaysFreeType = false,
-                    RateADS = false,
-                    gdsApply = "NNNN",
-                    ComADS = 0,
-                    ComGDS = 0,
-                    ComOnePage = 0,
-                    ComPortal = 0,
-                    WaitListAvailable = false,
-                    hotelPayment = false,
-                    IsPromo = true,
-                    DiscountLevel = GetDiscountApplicationMode(offer.Discount.ApplicationMode),
-                    TipoDescuento = 1, //Porcentaje. TODO: Monto
-                    IdDictionaryDescription = Dictionary.Insert(offer.Name.Esp, offer.Name.Eng),
-                    IdDiccShortDesc = Dictionary.Insert(offer.Description.Esp, offer.Description.Eng),
-                    idDiccPromoDesc = Dictionary.Insert("", ""), //Valor Agregado
-                    Deleted = false
-                };
+                    transaction.Rollback();
+                    return new KeyValuePair<string, string>("0", "Add: " + e.Message);
+                }
             }
             return new KeyValuePair<string, string>("1", "success");
         }
