037f07458c6bc65b4915452d68cf9ba245301582
Carrera <ricardo@internetpowerhotel.com>
2024-06-12 11:09:19 -0700

    Fix

--- APIServices/Conflux/Helpers/Rates/RateAmountMessage.cs
+++ APIServices/Conflux/Helpers/Rates/RateAmountMessage.cs
@@ -130,7 +130,7 @@ namespace APIServices.Conflux.Helpers.Rates
             if (pricesException.Count > 0)
             {
                 Rate rateException = new Rate();
-                rate.CurrencyCode = RatesHelpers.Currency;
+                rateException.CurrencyCode = RatesHelpers.Currency;
                 rateException.HasPriceException = true;
                 rateException.StartDate = starDate.Date.ToString("yyyyMMdd");
                 rateException.EndDate = endDate.Date.ToString("yyyyMMdd");
@@ -283,7 +283,7 @@ namespace APIServices.Conflux.Helpers.Rates
             if (pricesException.Count > 0)
             {
                 Rate rateException = new Rate();
-                rate.CurrencyCode = RatesHelpers.Currency;
+                rateException.CurrencyCode = RatesHelpers.Currency;
                 rateException.HasPriceException = true;
                 rateException.StartDate = starDate.Date.ToString("yyyyMMdd");
                 rateException.EndDate = endDate.Date.ToString("yyyyMMdd");
@@ -428,7 +428,7 @@ namespace APIServices.Conflux.Helpers.Rates
             if (pricesException.Count > 0)
             {
                 Rate rateException = new Rate();
-                rate.CurrencyCode = RatesHelpers.Currency;
+                rateException.CurrencyCode = RatesHelpers.Currency;
                 rateException.HasPriceException = true;
                 rateException.StartDate = starDate.Date.ToString("yyyyMMdd");
                 rateException.EndDate = endDate.Date.ToString("yyyyMMdd");
@@ -589,7 +589,7 @@ namespace APIServices.Conflux.Helpers.Rates
             if (pricesException.Count > 0)
             {
                 Rate rateException = new Rate();
-                rate.CurrencyCode = RatesHelpers.Currency;
+                rateException.CurrencyCode = RatesHelpers.Currency;
                 rateException.HasPriceException = true;
                 rateException.StartDate = startDate.Date.ToString("yyyyMMdd");
                 rateException.EndDate = endDate.Date.ToString("yyyyMMdd");
